39c012fb240de8c94b94506960d40551fdf34dcb
xiaotong chen <xiaotongchen@xiaotong-chens-MacBook-Pro.local>
2023-03-21 09:31:24 -0700

    update scripts

--- scripts/1_relative_appeal_score_comparison.sh
+++ scripts/1_relative_appeal_score_comparison.sh
@@ -3,7 +3,6 @@
 name="pair_with_clip_$1"
 root="datasets/$1"
 copy_from="outputs/pair_with_clip_$1/scores_real_all.txt"
-echo "$name $root $copy_from"
 
 python train.py --name $name --loss_type pair --root $root --image_size 512 --batch_size 16 --gpus 2 --num_epochs 10 --lr 1e-3
 python train.py --name $name --loss_type pair --root $root --image_size 512 --batch_size 16 --gpus 2 --num_epochs 10 --lr 1e-5 --unfreeze_pretrained --resume ckpts/$name/last.ckpt
--- scripts/2_appeal_score_prediction.sh
+++ scripts/2_appeal_score_prediction.sh
@@ -2,7 +2,6 @@
 
 name="singular_with_clip_$1"
 root="datasets/$1"
-echo "$name $root"
 
 python train.py --name $name --loss singular --root $root --image_size 512 --batch_size 16 --gpus 2 --num_epochs 10 --lr 1e-3
 python train.py --name $name --loss singular --root $root --image_size 512 --batch_size 16 --gpus 2 --num_epochs 10 --lr 1e-5 --unfreeze_pretrained --resume ckpts/$name/last.ckpt
